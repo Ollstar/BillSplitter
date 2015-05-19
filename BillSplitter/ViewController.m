33f28e2d7823504e67a465280c6faff2d8c1e49e
Oliver Andrews <oltandrews@gmail.com>
2015-05-19 07:29:30 -0700

    fin

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (nonatomic) NSDecimalNumber *splitBill;
+@property (nonatomic) NSDecimalNumber *bill;
+@property (nonatomic) NSDecimalNumber *payeeCount;
+@property (nonatomic) NSNumberFormatter *formatter;
 
 @end
 
@@ -17,11 +25,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.textField.delegate = self;
+    [self.slider addTarget:self action:@selector(moveSlider:) forControlEvents:UIControlEventValueChanged];
+    self.formatter = [[NSNumberFormatter alloc]init];
+//    [self.formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resoufrces that can be recreated.
+}
+- (IBAction)button:(id)sender {
+    [self.textField resignFirstResponder];
+    
+    self.bill = [NSDecimalNumber decimalNumberWithString:self.textField.text];
+    self.splitBill = [self.bill decimalNumberByDividingBy:self.payeeCount];
+    
+    self.label.text = [self.splitBill stringValue];
+    
+    self.label.text = [NSNumberFormatter ]
+}
+- (IBAction)moveSlider:(UISlider *)sender {
+    self.payeeCount = [[NSDecimalNumber alloc]initWithFloat:sender.value];
 }
 
 @end
